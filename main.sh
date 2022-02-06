0804fa4f6b595f5ea0f2aa23029a8a4fce61f18d
Nicholas Ng <onenylxus@gmail.com>
2022-02-06 18:05:10 +0800

    Mount new partition

--- main.sh
+++ main.sh
@@ -15,6 +15,13 @@ lfsVariable() {
   proceed
 }
 
+mountPartition() {
+  clear
+  echo -e "Run mount-partition.sh (LFS 2.7 - Mounting the New Partition)\n"
+  bash src/mount-partition.sh
+  proceed
+}
+
 proceed() {
   read -n 1 -s -r -p $'\n[v] Press any key to continue'
 }
@@ -32,12 +39,14 @@ do
   echo "Select action:"
   echo "[1] Run version-check.sh (LFS 2.2 - Host System Requirements)"
   echo "[2] Run lfs-variable.sh (LFS 2.6 - Setting The \$LFS Variable)"
+  echo "[3] Run mount-partition.sh (LFS 2.7 - Mounting the New Partition)"
   echo -e "[q] Exit menu\n"
   read -n 1 answer
 
   case "$answer" in
     1) versionCheck;;
     2) lfsVariable;;
+    3) mountPartition;;
     q) quit;;
   esac
 done
