08ab9fb10c3f59ebf96bd5567b9d23a9776c1278
Nicholas Ng <onenylxus@gmail.com>
2022-02-06 18:03:44 +0800

    Set and check $LFS variable

--- main.sh
+++ main.sh
@@ -8,6 +8,13 @@ versionCheck() {
   proceed
 }
 
+lfsVariable() {
+  clear
+  echo -e "Run lfs-variable.sh (LFS 2.6 - Setting The \$LFS Variable)\n"
+  bash src/lfs-variable.sh
+  proceed
+}
+
 proceed() {
   read -n 1 -s -r -p $'\n[v] Press any key to continue'
 }
@@ -24,11 +31,13 @@ do
   echo -e "LFS version 11.0, BLFS version 11.0\n"
   echo "Select action:"
   echo "[1] Run version-check.sh (LFS 2.2 - Host System Requirements)"
+  echo "[2] Run lfs-variable.sh (LFS 2.6 - Setting The \$LFS Variable)"
   echo -e "[q] Exit menu\n"
   read -n 1 answer
 
   case "$answer" in
     1) versionCheck;;
+    2) lfsVariable;;
     q) quit;;
   esac
 done
